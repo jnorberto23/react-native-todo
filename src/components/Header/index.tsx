43363c003215b0a7ef5d2d6b4ddae8ca5d6b2e68
jnorberto23 <joaovictor.norberto@outlook.com>
2023-09-26 17:45:01 -0300

    ✨add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import {Image, StyleSheet, View} from 'react-native';
+import { colors } from '../../config/colors';
+
+export default function Header() {
+  return (
+      <View style={styles.container}>
+       <Image source={require('../../assets/logo.png')} />
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 150,
+    backgroundColor: colors.gray900,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 20,
+    paddingBottom: 30,
+  },
+});
